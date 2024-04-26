5537707cf865add9cafe070afa0426fe8d5c8da8
ant-er <164273637+ant-er@users.noreply.github.com>
2024-04-26 17:40:31 -0400

    Create meme_bot.py

---
+++ meme_bot.py
@@ -0,0 +1,54 @@
+import discord
+from discord.ext import commands
+import os
+print(os.listdir('images'))
+import random
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def mem(ctx):
+    todas_las_imagenes = os.listdir("images")
+    img_name = random.choice(todas_las_imagenes)
+    with open(f'images/{img_name}', 'rb') as f:
+            picture = discord.File(f)
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('dog')
+async def dog(ctx):
+    image_url2 = get_dog_image_url()
+    await ctx.send(image_url2)
+
+bot.run("MTIyNjU4NTI5MTkyNjY3MTQ2MA.Gb_R1-.AXPaXrXgglcOQBt0HtfsxHCjNhfluYcybtSzIA")
